9da26b1d4bf91afe927006f4179ee48b6cc15da9
may <bankouzhenyou8@gmail.com>
2024-08-25 15:12:58 +0900

    スキル欄デザイン変更

--- app/Skill/index.tsx
+++ app/Skill/index.tsx
@@ -1,22 +1,19 @@
 import type { FC } from 'react';
 import { SkillCard } from './components/SkillCard';
 import { skillsData } from './assets/skillsData';
-import { jersey_10 } from '../ui/fonts';
 
 export const Skill: FC = () => {
   return (
-    <section>
-      <div className="flex items-baseline gap-4">
-        <h2
-          className={`mb-5 mt-12 text-2xl font-semibold text-primary ${jersey_10.className}`}
-        >
-          Skills
-        </h2>
-      </div>
+    <section className="grid">
+      <h2 className="mt-12 text-4xl font-semibold text-primary mb-5">
+        # Skills
+      </h2>
       <div className="grid gap-x-3 gap-y-3 sm:grid-cols-3">
         <SkillCard skillsData={skillsData} />
       </div>
-      <a className="text-muted-foreground text-lg">read more →</a>
+      <a className="text-lg text-muted-foreground text-right mr-1 mt-1">
+        👉 read more
+      </a>
     </section>
   );
 };
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,3 @@
 import { Inter } from 'next/font/google';
-import { Jersey_10 } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
-export const jersey_10 = Jersey_10({ weight: '400', subsets: ['latin'] });
